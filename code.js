c77a0b5da49d4118d0e91524219d22f8ecbfd042
Yertle75 <39764905+Yertle75@users.noreply.github.com>
2020-11-06 17:55:05 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name     Visits Tracker
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    
+    // Return null if not found
+    return null;
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000) - d.getTime()%(24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+var x = getCookie('nb_visits')
+if (x == null) {
+	setCookie('nb_visits',1,1);
+}
+else {
+	setCookie('nb_visits',Number(x)+1,1);
+}
